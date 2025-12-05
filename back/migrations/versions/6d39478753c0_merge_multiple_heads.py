49eda0f785c3afbe31c1c0d5ba6675fc13138947
Larry Mota--Lavigne <larry.motalavigne@gmail.com>
2025-12-05 14:50:07 +0100

    Update migration to fix `down_revision` reference

--- back/migrations/versions/6d39478753c0_merge_multiple_heads.py
+++ back/migrations/versions/6d39478753c0_merge_multiple_heads.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '6d39478753c0'
-down_revision: Union[str, None] = ('004', '8e5a401deec3')
+down_revision: str = '005_remove_forwarded_to'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
